00d0de52d945d72a959209eeb2122ada717db621
Abilkhaiyr Taibekov <abilkhaiyr.taibekov@nu.edu.kz>
2019-07-02 22:26:21 +0600

    deleted root dir & style changes🔮sese🔮

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -93,10 +93,6 @@ const webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
-      },
-      {
-        from: path.resolve(__dirname, '../root'),
-        to: config.build.assetsRoot
       }
     ])
   ]
