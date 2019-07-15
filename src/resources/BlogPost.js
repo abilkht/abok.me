47996ca974ee910022213c96023aa39d4ccff8f6
Abilkhaiyr Taibekov <abilkhaiyr.taibekov@nu.edu.kz>
2019-07-15 11:51:53 +0600

    removed temp second post❌

--- src/resources/BlogPost.js
+++ src/resources/BlogPost.js
@@ -5,7 +5,6 @@ export default {
       resolve: (response, mappers) => {
         let { title, content, meta } = response.results[0];
         content = '<p>' + content.split('\n').join('</p><p>') + '</p>';
-        content = content.split('tochka').join('<br>');
         return mappers.merge({ title, content, ...meta })
       }
     }
